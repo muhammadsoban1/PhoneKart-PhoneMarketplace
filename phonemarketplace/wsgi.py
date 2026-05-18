a84cf6d9af8b2018870eafef01cf7fa09e8c9ac5
Muhammad soban <sobanmuhammad48@gmail.com>
2026-05-19 00:18:06 +0500

    Updated wsgi.py and configurations for vercel

--- phonemarketplace/wsgi.py
+++ phonemarketplace/wsgi.py
@@ -14,3 +14,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'phonemarketplace.settings')
 
 application = get_wsgi_application()
+
+# Vercel ko is 'app' variable ki zaroorat hoti hai deployment ke liye
+app = application
